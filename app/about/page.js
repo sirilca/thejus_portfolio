6a1f61510a9d98f1d4bf614b6028db1b6ee0959f
sirilca <sirilcantony@gmail.com>
2024-02-11 00:59:00 +0530

    added 127.0.1 instead of localhost-testusestate

--- app/about/page.js
+++ app/about/page.js
@@ -1,38 +1,56 @@
+'use client'
+
 import AboutSection from "@/components/AboutSection";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 
 async function getProjects() {
-    try {
-        const res = await fetch('/api/data', { cache: 'no-store' })
-        if (!res.ok) {
-            throw new Error(`Failed to fetch projects: ${res.statusText}`);
-        }
-        const data = await res.json()
-        // console.log(data)
-        if(data){
 
-            return "data vanoota  "
-        }
-        else{
-            return "no way mone"
-        }
+    // try {
+    //     const res = await fetch('/api/data', { cache: 'no-store' })
+    //     if (!res.ok) {
+    //         throw new Error(`Failed to fetch projects: ${res.statusText}`);
+    //     }
+    //     const data = await res.json()
+    //     // console.log(data)
+    //     if(data){
 
-    }
-    catch (err) {
-        return "hiii"
-    }
+    //         return "data vanoota  "
+    //     }
+    //     else{
+    //         return "no way mone"
+    //     }
+
+    // }
+    // catch (err) {
+    //     return "hiii"
+    // }
 }
 
 
-const About = async () => {
+const About = () => {
 
-    const data= await getProjects()
+    const [data,setData]= useState()
+
+    useEffect(()=>{
+        axios.get('http://localhost:3000/api/data').then(res => {
+            // console.log(res.data)
+            setData(res.data)
+        }).catch(err => {
+            return 'hiii'
+        })
+    },[])
+
+    
+    // const data= await getProjects()
     // const finddata=data[0].aboutsection
 
 
   return (
       <div className=''>
-    {data?<>{data}</>:<></>}
+        
+    {data?<>{data[0].biography.description}</>:<>no data mone  </>}
         {/* {finddata?<h1>{finddata.description}</h1>:<>no data we got</>} */}
 
         div vannille
