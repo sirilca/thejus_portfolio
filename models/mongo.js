070256928d55aab107b704fa014bccab92ff4c42
sirilca <sirilcantony@gmail.com>
2024-02-10 23:21:12 +0530

    fll error-2

--- models/mongo.js
+++ models/mongo.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectionString = process.env.MONGO_CONNECTION_STRING;
 
-mongoose.connect(connectionString).then(() => {console.log("mongo connected")});
+mongoose.connect('mongodb+srv://thejusjoseph:dwMMlpOhly7HFKgx@cluster0.2mkkvws.mongodb.net/portfolio?retryWrites=true&w=majority').then(() => {console.log("mongo connected")});
 // mongoose.connect(connectionString).then(() => { console.log("MongoDB connected") });
 
 const DataModel = mongoose.models['data'] || mongoose.model('data', new mongoose.Schema({}));
